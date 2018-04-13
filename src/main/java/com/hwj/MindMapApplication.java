4185b3d5432256c68f7d54704dcb33aafba36acf
Ragty <961946407@qq.com>
2018-04-13 11:45:11 +0800

    pom.xml文件修改

--- src/main/java/com/hwj/MindMapApplication.java
+++ src/main/java/com/hwj/MindMapApplication.java
@@ -2,12 +2,20 @@ package com.hwj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement     //开启事物注解
-public class MindMapApplication {
+public class MindMapApplication extends SpringBootServletInitializer{
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(MindMapApplication.class);
+    }
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(MindMapApplication.class, args);
 	}
