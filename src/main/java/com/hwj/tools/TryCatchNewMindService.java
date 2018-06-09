23cd3f8820235532c30afa4101e3dc69f9052e64
Ragty <961946407@qq.com>
2018-06-09 21:28:41 +0800

    根据字段获取NewMind

--- src/main/java/com/hwj/tools/TryCatchNewMindService.java
+++ src/main/java/com/hwj/tools/TryCatchNewMindService.java
@@ -78,4 +78,27 @@ public class TryCatchNewMindService {
 	
 	
 	
+	/**
+	 * @author Ragty
+	 * @param  根据两个属性获取MindMap
+	 * @serialData 2018.6.9
+	 * @param propertyName1
+	 * @param value1
+	 * @param propertyName2
+	 * @param value2
+	 * @return
+	 */
+	public MindMap getMindMap(String propertyName1,Object value1,String propertyName2,Object value2){
+		
+		MindMap mindMap = null;
+		try {
+			mindMap = iMindMapService.get(propertyName1, propertyName2, value1, value2);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+		return mindMap;
+	}
+	
+	
 }
