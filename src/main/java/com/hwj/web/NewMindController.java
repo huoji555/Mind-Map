4056fe480003c0f9b1dbff61970ae0a701ff6f32
Ragty <961946407@qq.com>
2018-06-13 11:52:15 +0800

    节点上传文件接口

--- src/main/java/com/hwj/web/NewMindController.java
+++ src/main/java/com/hwj/web/NewMindController.java
@@ -2,6 +2,8 @@ package com.hwj.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.sql.Blob;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,6 +16,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hwj.entity.FileCollection;
 import com.hwj.entity.FileShare;
@@ -33,10 +37,13 @@ import com.hwj.entity.MindNode;
 import com.hwj.entity.Share;
 import com.hwj.entity.UploadFile;
 import com.hwj.entity.Zsd;
+import com.hwj.entityUtil.BeSaveFileUitl;
 import com.hwj.entityUtil.MindNodeTool;
 import com.hwj.entityUtil.Node2;
 import com.hwj.json.JsonAnalyze;
+import com.hwj.tools.FileUpload;
 import com.hwj.tools.StatusMap;
+import com.hwj.tools.StreamToBlob;
 import com.hwj.tools.TryCatchFileCollectionService;
 import com.hwj.tools.TryCatchFileShareService;
 import com.hwj.tools.TryCatchFileStreamService;
@@ -68,6 +75,10 @@ public class NewMindController {
 	private TryCatchShareService tryCatchShareService;
 	@Autowired
 	private TryCatchUserService tryCatchUserService;
+	@Autowired
+	private FileUpload fileUpload;
+	@Autowired
+	private StreamToBlob streamToBlob;
 	@Autowired
 	private JsonAnalyze jsonAnalyze;
 	@Autowired
@@ -1147,7 +1158,212 @@ public class NewMindController {
 	
 	
 	
-	
+	/**
+	 * @author Ragty
+	 * @param  节点上传文件
+	 * @param nodeid
+	 * @param rootid
+	 * @param file
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping("setMapUpload.do")
+	@ResponseBody
+	public String setMapUpload(@RequestParam("nodeid") String nodeid,
+			@RequestParam("rootid") String rootid,
+			@RequestParam("wenjian") MultipartFile file,
+			HttpServletRequest request, HttpServletResponse response) throws IOException{
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+		String uploadtima = df.format(new Date());
+		HttpSession session = request.getSession();
+		String userid = String.valueOf(session.getAttribute("username"));
+		
+		MindMap mindMap = tryCatchNewMindService.getMindMap("nodeid", rootid);
+	    String mindUser = mindMap.getUserid();
+		
+		if (userid.equals("null") || userid.equals(null)) {
+			return statusMap.a("2");
+		}
+		
+		//禁止非本节点用户在节点上传文件
+		if (! (userid.equals(mindUser)) ){
+			System.out.println("到这里,,,,,");
+			return statusMap.a("5");
+		}
+		
+		SimpleDateFormat df2 = new SimpleDateFormat("yyyyMMddHHmmss");
+		String files = df2.format(new Date());
+		String filename = file.getOriginalFilename();
+		
+		String zsdid = null;
+		String zlid = null;
+		try {
+			UploadFile uploadFile = tryCatchUploadFileService.getUploadFile(
+					"userid", "filename", "firstStatus", userid, filename, "1");
+			zsdid = uploadFile.getZsdid();
+			zlid = uploadFile.getFiles();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		//判断资源页面有无上传过，没上传过的话（直接上传），否则直接添加数据
+		if (tryCatchUploadFileService.haveUploadFile("userid", "filename",
+				userid, filename)) {
+			
+			if(zsdid == null){
+				UploadFile uploadFile = tryCatchUploadFileService.getUploadFile(
+						"userid", "filename", "firstStatus", userid, filename, "1");
+				
+				uploadFile.setZsdid(nodeid);
+				uploadFile.setFirstStatus("0");
+				
+				UploadFile uploadFile1 = tryCatchUploadFileService
+						.getUploadFile("userid", "filename", "firstStatus",
+								userid, filename, "1");
+				uploadFile1.setZsdid("1");
+				
+				if (tryCatchUploadFileService.saveUploadFile(uploadFile)) {
+					System.out.println("资源界面已经上传过该文件");
+					tryCatchUploadFileService.updateUploadeFile(uploadFile1);
+					return statusMap.a("1");
+				} else {
+					return statusMap.a("2");
+				}
+				
+				
+			} else if (zsdid != nodeid) {
+
+				UploadFile uploadFile = tryCatchUploadFileService
+						.getUploadFile("userid", "filename", userid, filename);
+				uploadFile.setZsdid(nodeid);
+				uploadFile.setFirstStatus("0");
+
+				if (tryCatchUploadFileService.haveUploadFile("files", "zsdid",
+						zlid, nodeid)) {
+					System.out.println("防止在该节点重复上传文件");
+					return statusMap.a("3");
+				}
+
+				if (tryCatchUploadFileService.saveUploadFile(uploadFile)) {
+					System.out.println("在新节点上传该文件");
+					return statusMap.a("1");
+				} else {
+					return statusMap.a("2");
+				}
+
+			}
+			
+		}
+		
+		
+		//直接上传文件
+		String type = "";
+
+		String fileExtension = file.getOriginalFilename().substring(
+				file.getOriginalFilename().lastIndexOf(".") + 1,
+				file.getOriginalFilename().length());
+		if ((fileExtension.equals("bmp")) || (fileExtension.equals("drw"))
+				|| (fileExtension.equals("dxf"))
+				|| (fileExtension.equals("eps"))
+				|| (fileExtension.equals("gif"))
+				|| (fileExtension.equals("jpg"))
+				|| (fileExtension.equals("png"))
+				|| (fileExtension.equals("pcd"))
+				|| (fileExtension.equals("pcx"))) {
+			type = "picture";
+		} else if ((fileExtension.equals("avi"))
+				|| (fileExtension.equals("mpeg"))
+				|| (fileExtension.equals("mpg"))
+				|| (fileExtension.equals("dat"))
+				|| (fileExtension.equals("ra")) || (fileExtension.equals("rm"))
+				|| (fileExtension.equals("wmv"))
+				|| (fileExtension.equals("mp4"))
+				|| (fileExtension.equals("swf"))
+				|| (fileExtension.equals("f4v"))) {
+			type = "video";
+		} else if ((fileExtension.equals("cd"))
+				|| (fileExtension.equals("ogg"))
+				|| (fileExtension.equals("mp3"))
+				|| (fileExtension.equals("asf"))
+				|| (fileExtension.equals("wma"))
+				|| (fileExtension.equals("wav"))
+				|| (fileExtension.equals("rm"))
+				|| (fileExtension.equals("midi"))
+				|| (fileExtension.equals("ape"))) {
+			type = "doc";
+		} else {
+			type = "other";
+		}
+		byte[] newsPageByte = file.getBytes();
+		String realPath = request.getSession().getServletContext()
+				.getRealPath("/");
+
+		String fileURL = realPath + "upload";
+
+		BeSaveFileUitl be = new BeSaveFileUitl();
+		be.setFileExtension(fileExtension);
+		be.setFilesByte(newsPageByte);
+		be.setFileURL(fileURL);
+
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		System.out.println("ip" + ip);
+
+		String Url2 = "";
+		String Url3 = "";
+		String[] string = this.fileUpload.saveFile(be);
+
+		//代表文件上传成功
+		if ("1".equals(string[0])) {
+			String URL = string[1];
+			// 文件的真实路径，将之代替截图路径存入
+			String url = URL.replaceAll("\\\\", "/") + "." + fileExtension;
+			String real = realPath.replaceAll("\\\\", "/");
+			
+			Url3 = url; // 文件在服务器中的真实路径，用来删除
+			Url2 = "http://"  + ip + ":8080/"+ url.split(real)[1]; //代表服务器中的地址
+
+		}
+		
+		Blob blob = streamToBlob.toBlob(file.getInputStream());
+
+		// 数据备份
+		FileStream fileStream = new FileStream();
+		fileStream.setFilename(filename);
+		fileStream.setF_id(files);
+		fileStream.setFileExtension(fileExtension);
+		fileStream.setParentid("0");
+		fileStream.setUserid(userid);
+		fileStream.setTrueUrl(Url2);
+		fileStream.setFileStream(blob);
+		fileStream.setFileType(type);
+		fileStream.setDelStatus("0");
+		fileStream.setNodeid(nodeid);
+		fileStream.setUploadTime(uploadtima);
+
+		tryCatchFileStreamService.saveFileStream(fileStream);
+
+		UploadFile uploadFile = new UploadFile();
+		uploadFile.setFilename(filename);
+		uploadFile.setFiles(files); // 文件id
+		uploadFile.setFilepath(Url2); // 文件的输出路径（在服务器上的访问路径）
+		uploadFile.setFileroot(Url3); // 文件在pc上的实际路径
+		uploadFile.setZlms("该资料现在没有描述");
+		uploadFile.setFiletype(type);
+		uploadFile.setUploadtime(uploadtima);
+		uploadFile.setZsdid(nodeid);
+		uploadFile.setUserid(userid);
+		uploadFile.setF_parentid("0");
+		uploadFile.setFirstStatus("1");
+		if (this.tryCatchUploadFileService.saveUploadFile(uploadFile)) {
+			System.out.println("第一次在节点上传该文件");
+			return statusMap.a("1");
+		}
+		return statusMap.a("2");
+		
+	}
 	
 	
 	
