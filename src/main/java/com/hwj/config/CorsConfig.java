020b2ecc559909a21fa90bab85644669cbc87227
huoji555 <huoji566@gmail.com>
2019-03-29 16:30:43 +0800

    增加接口跨域配置
    Signed-off-by: huoji555 <huoji566@gmail.com>

---
+++ src/main/java/com/hwj/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.hwj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Author Ragty
+ * @Description  跨域配置
+ * @Date 9:37 2019/3/5
+ * @return
+ **/
+@Configuration
+public class CorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+
+
+}
